871a0c8584922c6d4f684f240ad07ba8b405acf4
Max Kuzkin <maximk@odin.com>
2016-08-29 18:31:35 +0300

    added support for filtering with hardcoded filter for now

--- gcpmetrics/gcpmetrics.py
+++ gcpmetrics/gcpmetrics.py
@@ -42,7 +42,68 @@ def list_metric_descriptors(client):
         print '\tdescription: {}'.format(descriptor.description.encode('utf-8'))
         print
 
-def metrics(project_id, resource_descriptors, metric_descriptors):
+def _build_label_filter(category, *args, **kwargs):
+    """Construct a filter string to filter on metric or resource labels."""
+    terms = list(args)
+    print 'PATCHED _build_label_filter (!)'
+    import six
+    for key, value in six.iteritems(kwargs):
+        if value is None:
+            continue
+
+        suffix = None
+        if key.endswith('_prefix') \
+            or key.endswith('_suffix') \
+            or key.endswith('_greater') \
+            or key.endswith('_greaterequal') \
+            or key.endswith('_less') \
+            or key.endswith('_lessequal'):
+                key, suffix = key.rsplit('_', 1)
+
+        if category == 'resource' and key == 'resource_type':
+            key = 'resource.type'
+        else:
+            key = '.'.join((category, 'label', key))
+
+        if suffix == 'prefix':
+            term = '{key} = starts_with("{value}")'
+        elif suffix == 'suffix':
+            term = '{key} = ends_with("{value}")'
+        elif suffix == 'greater':
+            term = '{key} > {value}'
+        elif suffix == 'greaterequal':
+            term = '{key} >= {value}'
+        elif suffix == 'less':
+            term = '{key} < {value}'
+        elif suffix == 'lessequal':
+            term = '{key} <= {value}'
+        else:
+            term = '{key} = "{value}"'
+
+        terms.append(term.format(key=key, value=value))
+
+    return ' AND '.join(sorted(terms))
+
+def perform_query(client):
+
+    # metric.type = "appengine.googleapis.com/http/server/response_count"
+    #   AND resource.label.module_id = "service"
+    #   AND metric.label.response_code >= 500
+    #   AND metric.label.response_code < 600
+
+    # yes, this is ugly, but we need to fix this method...
+    monitoring.query._build_label_filter = _build_label_filter
+
+    query = client.query(
+        metric_type='appengine.googleapis.com/http/server/response_count',
+        days=10
+        )
+
+    query = query.select_resources(module_id='service')
+    query = query.select_metrics(response_code_greaterequal=500)
+    print query.filter
+
+def metrics(project_id, resource_descriptors, metric_descriptors, query):
     client = monitoring.Client(project=project_id)
 
     if resource_descriptors:
@@ -50,6 +111,9 @@ def metrics(project_id, resource_descriptors, metric_descriptors):
 
     elif metric_descriptors:
         list_metric_descriptors(client)
+
+    elif query:
+        perform_query(client)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -62,9 +126,11 @@ def main():
         '--resource_descriptors', default=False, action='store_true', help='List monitored resource descriptors.')
     parser.add_argument(
         '--metric_descriptors', default=False, action='store_true', help='List available metric descriptors.')
+    parser.add_argument(
+        '--query', default=False, action='store_true', help='Perform time series query.')
 
     args = parser.parse_args()
-    metrics(args.project_id, args.resource_descriptors, args.metric_descriptors)
+    metrics(args.project_id, args.resource_descriptors, args.metric_descriptors, args.query)
 
 if __name__ == '__main__':
     sys.exit(main())
