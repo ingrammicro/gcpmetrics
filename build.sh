8eb456a94be741dce554d903e86346c416fb4c18
Max Kuzkin <maximk@odin.com>
2016-09-01 00:58:41 +0300

    skip flake in travis - it will be launched on test stage

--- build.sh
+++ build.sh
@@ -21,22 +21,23 @@ PACKAGE_VERSION="1.0.$BUILD_NUMBER"
 echo "Generating VERSION file ($PACKAGE_VERSION)..."
 echo "$PACKAGE_VERSION" > VERSION
 
-# version information is here for logging purposes:
-# python 2.7+ is required to package properly
-python --version
-flake8 --version
-
-# nice color constants
-RED_COLOR='\033[0;31m'
-NO_COLOR='\033[0m'
-
-# check syntax (and highlight output)
-printf "${RED_COLOR}"
-flake8 .
-printf "${NO_COLOR}"
-
 if [ -z "$TRAVIS_BUILD_NUMBER" ]
 then
+    # version information is here for logging purposes:
+    # python 2.7+ is required to package properly
+    python --version
+    flake8 --version
+
+    # nice color constants
+    RED_COLOR='\033[0;31m'
+    NO_COLOR='\033[0m'
+
+    # check syntax (and highlight output)
+    printf "${RED_COLOR}"
+    flake8 .
+    printf "${NO_COLOR}"
+
     # build doc and src packages
     python setup.py publish $@
 fi
+
